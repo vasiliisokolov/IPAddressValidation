c2d70a19dccc1c505358a74d3b2d7e2c179531f1
Vasily Sokolov <vasilysokolov@mail.ru>
2022-08-11 14:04:15 +0300

    new functions

--- IPAddressValidation/IPAddressValidation.cpp
+++ IPAddressValidation/IPAddressValidation.cpp
@@ -1,33 +1,129 @@
 ﻿#include <iostream>
+#include <stdlib.h>
 
 bool check_format(std::string ip);
 std::string get_address_part(std::string ip, int oktetNumber);
+bool check_part(std::string part);
 
 int main()
 {
     std::string ip;
+    int oktetNumber;
     bool valid = true;
     std::cout << "IP Address Validation!\n";
     std::cout << "Enter your IP:\n";
     std::cin >> ip;
 
-    valid = check_format(ip);
+    if (check_format(ip))
+    {
+        for (oktetNumber = 0; oktetNumber <= 3; oktetNumber++)
+        {
+            if (!check_part(get_address_part(ip, oktetNumber)))
+            {
+                valid = false;
+                break;
+            }
+        }
+    }
+    else
+    {
+        valid = false;
+    }
     std::cout << (valid ? "Valid\n": "Invalid\n");
 }
 
 bool check_format(std::string ip)
 {
-    for (int i = 0; i > ip.length(); i++)
+    if (ip.find("..") == -1)
     {
-        if (ip[i] == '.' || ip[i] > '0' && ip[i] < '9')
+        for (int i = 0; i < ip.length(); i++)
         {
-            return true;
+            if (ip[i] != '.' || ip[i] > '0' && ip[i] > '9')
+            {
+                return false;
+            }
         }
     }
-    return false;
+    else
+    {
+        return false;
+    }
+    return true;
 }
 
 std::string get_address_part(std::string ip, int oktetNumber)
 {
-    return std::string();
+    
+    std::string part1, part2, part3, part4, temp;
+    std::string result;
+    
+    int number = 0;
+    for (int i = 0; i < ip.length(); i++)
+    {
+        if (ip[i] != '.')
+        {
+            switch (number)
+            {
+            case 0:
+                part1 += ip[i];
+                break;
+            case 1:
+                part2 += ip[i];
+                break;
+            case 2:
+                part3 += ip[i];
+                break;
+            case 3:
+                part4 += ip[i];
+                break;
+            }
+        }
+        else if (ip[i] == '.')
+        {
+            number++;
+            temp.clear();
+
+        }
+
+    }
+
+    switch (oktetNumber)
+    {
+    case 0:
+        result = part1;
+        break;
+    case 1:
+        result = part2;
+        break;
+    case 2:
+        result = part3;
+        break;
+    case 3:
+        result = part4;
+        break;
+    }
+      
+    return std::string(result);
+}
+
+bool check_part(std::string part)
+{
+    
+    if (part.length() > 2)
+    {
+        return false;
+    }
+    else
+    {
+        int number = atoi(part.c_str());
+        if (number >= 0 && number <= 225)
+        {
+            return true;
+        }
+        else
+        { 
+            return false;
+        }
+    }
+
 }
